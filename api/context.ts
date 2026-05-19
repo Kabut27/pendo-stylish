fb2b0d7ee00366a61b767583b985f1fcc227ebe7
Kabut27 <86082951+Kabut27@users.noreply.github.com>
2026-05-19 16:26:39 +0300

    Update context.ts

--- api/context.ts
+++ api/context.ts
@@ -1,9 +1,18 @@
 import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 
-export async function createContext(opts: FetchCreateContextFnOptions) {
+export interface Env {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  DB: any;
+  JWT_SECRET: string;
+}
+
+export async function createContext(
+  opts: FetchCreateContextFnOptions & { env?: Env }
+) {
   return {
     req: opts.req,
     resHeaders: opts.resHeaders,
+    db: opts.env?.DB ?? null,
   };
 }
 
